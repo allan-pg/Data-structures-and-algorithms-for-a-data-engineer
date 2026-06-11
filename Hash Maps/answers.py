6f2beaa48af0fe89cd2a64bdc99bb8ee323997bc
Allan Mwangi <carsonallan71@gmail.com>
2026-06-11 13:16:42 +0300

    Added answers to top 5 question

--- Hash Maps/answers.py
+++ Hash Maps/answers.py
@@ -40,3 +40,34 @@ for fruit in fruits:
         
 print(count)
 ```
+
+---
+
+### 3. Return the first duplicate value.
+
+
+---
+
+### 4. Remove duplicate values while preserving order.
+
+```python
+# First approach
+duplicated_list = [1,2,3,1,2,4]
+
+# create an empty list and set- sets do not accept duplicates
+seen = set()
+deduplicated_list=[]
+
+# iterate through your list this is O(n) time complexity
+for num in duplicated_list:
+    """check if num is in seen and if not add it to a set then append your empty list
+       because a set doesnt keep order so you append elements in list to preserve order
+       - appending a list is O(1) constant time complexity since you are adding at the
+       end of the list so no element moves
+    """
+    if num not in seen:
+        seen.add(num)
+        deduplicated_list.append(num)
+        
+print(deduplicated_list)
+```
