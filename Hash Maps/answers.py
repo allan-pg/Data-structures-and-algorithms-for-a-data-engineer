eb3f26349364f6d7681f5b34d130a989dec86bb1
Allan Mwangi <carsonallan71@gmail.com>
2026-06-11 12:33:47 +0300

    Update hash map answers

--- Hash Maps/answers.py
+++ Hash Maps/answers.py
@@ -1,18 +1,42 @@
 
-### Remove Duplicates
+### 1. Count Character Frequency
 
-duplicated_list = [1,2,3,1,2,4]
-deduplicated = set()
-deduplicated_list = []
+```python
+s = 'banana'
+count = {}
 
-def deduplicate(num):
+# loop through each character in s this is O(n) linear time complexity 
+for char in s:
+    """ check if char is a key in count and if yes add 1 
+        to the value if no set the value as 1
+    """
 
-    for index,nums in enumerate(num):
-        if nums not in deduplicated:
-            deduplicated.add(nums)
-            deduplicated_list.append(nums)
+    if char in count:
+        count[char] += 1
+    else:
+        count[char] = 1
+        
+print(count)   
+```
 
-    print(deduplicated_list)
+---
 
-deduplicate(duplicated_list)   
+### 2. Count how many times each word appears.
 
+```python
+fruits = ["apple", "banana", "apple", "orange"]
+
+count = {}
+
+# loop though your list O(n) linear time complexity
+for fruit in fruits:
+    """ check if the key is in count e.g is apple in count and if yes 
+        add 1 to value if no set value as 1 """
+    
+    if fruit in count:
+        count[fruit] += 1
+    else:
+        count[fruit] = 1
+        
+print(count)
+```
